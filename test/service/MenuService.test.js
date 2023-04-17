b6b7e78dd5e9e1144340e24178269105c5835d7d
develckm <develckm@gmail.com>
2023-04-17 15:47:56 +0900

    수정완료

--- test/service/MenuService.test.js
+++ test/service/MenuService.test.js
@@ -1,29 +1,44 @@
-const MenuService=require("../../model/service/MenuService");
-const menuService=new MenuService();
+const menuService=require("../../model/service/MenuService");
+//const menuService=new MenuService();
 
 describe("MenuService test",()=>{
     test("findAllMenu",async ()=>{
-        const menus=await menuService.findAllMenu();
+        const menus=await menuService.findAllMenu(1);
         console.log(menus);
     });
 
-    test("findByMenu",async ()=>{
-        const menus=await menuService.findByMenu(1);
-        console.log(menus);
-    });
 
     test("insertMenu",async ()=>{
-        const menus=await menuService.insertMenu(2,"치즈닭갈비",null,11000,"매콤 고소 닭갈비","기존메뉴",1);
+        const menuObj={
+            name: "치즈닭갈비",
+            img: "img",
+            price: 20000,
+            info: "매콤 고소 닭갈비",
+            menu_type: "일반메뉴",
+            status: 1,
+            store_num: 1
+        }
+        const menus=await menuService.insertMenu(menuObj);
         console.log(menus);
     });
 
     test("modifyMenu",async ()=>{
-        const menus=await menuService.modifyMenu(2,3,"콘마요 닭갈비", null,12000,"매콤하지만 맛있는 닭갈비","기존메뉴",1);
+        const menuObj={
+            menu_num:2,
+            name: "콘마요 닭갈비",
+            img: "img",
+            price: 20000,
+            info: "매콤 고소 콘마요 닭갈비",
+            menu_type: "신메뉴",
+            status: 1,
+            store_num: 1
+        }
+        const menus=await menuService.modifyMenu(menuObj);
         console.log(menus);
     });
 
     test("dropMenu",async ()=>{
-        const menus=await menuService.dropMenu(2,4);
+        const menus=await menuService.dropMenu(6);
         console.log(menus);
     });
 
