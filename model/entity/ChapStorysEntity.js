0db032638e2f7d2006f98a8329412750fded3d1c
HUHMUMU <hurjun07@naver.com>
2023-05-02 10:37:07 +0900

    신고 링크 연결

--- model/entity/ChapStorysEntity.js
+++ model/entity/ChapStorysEntity.js
@@ -24,25 +24,10 @@ module.exports=(sequelize)=> {
             allowNull: false,
             comment: '조회수'
         },
-        date: {
-            type: DataTypes.DATEONLY,
+        post_time: {
+            type: DataTypes.TIME,
             allowNull: false,
             comment: '작성날짜'
-        },
-        likes: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            comment: '좋아요'
-        },
-        profile: {
-            type: DataTypes.STRING(255),
-            allowNull: true,
-            comment: '프로필'
-        },
-        main_img: {
-            type: DataTypes.STRING(255),
-            allowNull: true,
-            comment: '대표사진'
         },
         update_time: {
             type: DataTypes.DATE,
